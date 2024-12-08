845c318c3342808d8f161ab6460846248f257150
Nils Sohn <sohndotcloud@proton.me>
2024-12-07 17:05:14 -0700

    Loaded BMP file to canvas

--- falling-sand/src/main.c
+++ falling-sand/src/main.c
@@ -2,10 +2,13 @@
 #include "common.h"
 #include <SDL2/SDL.h>
 #include <SDL2/SDL_image.h>
+#include <unistd.h>
 
 int main(int argc, char *argv[])
 {
     SDL_Window *window = NULL;
+    SDL_Surface *windowSurface = NULL;
+    SDL_Surface *imageSurface = NULL;
 
     if (SDL_Init(SDL_INIT_VIDEO) < 0)
     {
@@ -21,13 +24,34 @@ int main(int argc, char *argv[])
         }
         else
         {
-            SDL_UpdateWindowSurface(window);
-            SDL_Delay(20000);
+            windowSurface = SDL_GetWindowSurface(window);
+            const char *newDir = SDL_GetBasePath();
+            if (chdir(newDir) != 0)
+            {
+                perror("chdir() failed");
+                SDL_Quit();
+                return -1;
+            }
+            imageSurface = SDL_LoadBMP("./test.bmp");
+
+            if (imageSurface == NULL)
+            {
+                printf("Image Loading Error, %s", SDL_GetError());
+            }
+            else
+            {
+                SDL_BlitSurface(imageSurface, NULL, windowSurface, NULL);
+                SDL_UpdateWindowSurface(window);
+                SDL_Delay(20000);
+            }
         }
     }
 
     SDL_DestroyWindow(window);
+    SDL_FreeSurface(imageSurface);
+    windowSurface = NULL;
+    imageSurface = NULL;
+    window = NULL;
     SDL_Quit();
-
     return 0;
 }
